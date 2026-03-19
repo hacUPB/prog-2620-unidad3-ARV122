c7087cebe9644eb73de0a463f481f8eb9a197359
Andres Ruiz <andres.ruizv@upb.edu.co>
2026-03-19 16:35:59 -0500

    Se agrega el ejercicio2 a la carpeta src

---
+++ src/ejercicio2_altura.py
@@ -0,0 +1,8 @@
+# Ejercicio 2: El guardián de la montaña rusa
+#Para subir a la nueva montaña rusa del parque, los visitantes deben medir al menos 150 cm.
+#Escribe un programa donde declares una variable altura_visitante (asígnale el valor que quieras). 
+#Luego, utiliza un operador relacional para imprimir True si puede subir o False si no puede.
+
+Altura_minima = 150 
+Altura_visitantes = 190 
+
